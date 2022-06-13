270f5da735bf82ebd8dee271ed78ef54540c0313
shozy786 <65444486+shozy786@users.noreply.github.com>
2022-06-13 17:10:15 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup( name="qt-async-threads",version="0.2",description="changes made to install this git using pip",
+    author="nicoddemus",
+    author_email="noidea@gmail.com",
+    url="https://github.com/nicoddemus/qt-async-threads",
+    license="MIT",
+    packages=["qt-async-threads"]
+    )
